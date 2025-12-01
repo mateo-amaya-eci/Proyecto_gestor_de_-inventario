893feb71f23e9157497ed903f509af290d612996
Mateoamaya <mateoamaya.2007@gmail.com>
2025-11-30 21:07:25 -0500

    correcion

---
+++ build/CMakeFiles/InventoryManager.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mateo_amaya/alse/ProyectoInventario/src/Component.cpp" "/home/mateo_amaya/alse/ProyectoInventario/build/CMakeFiles/InventoryManager.dir/src/Component.cpp.o"
+  "/home/mateo_amaya/alse/ProyectoInventario/src/DatabaseManager.cpp" "/home/mateo_amaya/alse/ProyectoInventario/build/CMakeFiles/InventoryManager.dir/src/DatabaseManager.cpp.o"
+  "/home/mateo_amaya/alse/ProyectoInventario/src/InventoryManager.cpp" "/home/mateo_amaya/alse/ProyectoInventario/build/CMakeFiles/InventoryManager.dir/src/InventoryManager.cpp.o"
+  "/home/mateo_amaya/alse/ProyectoInventario/src/MainWindow.cpp" "/home/mateo_amaya/alse/ProyectoInventario/build/CMakeFiles/InventoryManager.dir/src/MainWindow.cpp.o"
+  "/home/mateo_amaya/alse/ProyectoInventario/src/ReportGenerator.cpp" "/home/mateo_amaya/alse/ProyectoInventario/build/CMakeFiles/InventoryManager.dir/src/ReportGenerator.cpp.o"
+  "/home/mateo_amaya/alse/ProyectoInventario/src/main.cpp" "/home/mateo_amaya/alse/ProyectoInventario/build/CMakeFiles/InventoryManager.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
